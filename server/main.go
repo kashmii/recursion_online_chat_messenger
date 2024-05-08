ed1dd7e4ec2ea29f47f511a6f727ee3a05ed97da
kashmii <www.myac@gmail.com>
2024-05-08 09:26:28 +0900

    clientからの取得データを各パーツに分けて保存

--- server/main.go
+++ server/main.go
@@ -8,6 +8,8 @@ import (
 
 type ClientInfo struct {
     Address *net.UDPAddr
+    Username []byte
+    Message []byte
     ReceivedTime time.Time
 }
 
@@ -20,8 +22,6 @@ func main() {
         return
     }
 
-	fmt.Println(udpAddress.Port)
-
     // UDPのソケットを開く
 	// ListenUDP acts like ListenPacket for UDP networks.
     conn, err := net.ListenUDP("udp", udpAddress)
@@ -30,27 +30,30 @@ func main() {
         return
     }
     defer conn.Close()
+    fmt.Println("Starting UDP server on", udpAddress)
 
     // バッファを作成
-    buf := make([]byte, 1024)
+    buf := make([]byte, 4096)
     // クライアントの情報を保存するマップ
     clientInfos := make(map[string]*ClientInfo)
 
     for {
-        // データを読み取る
+        // UDPソケットから読み取ったデータをバイトスライス（buf）に格納
         n, addr, err := conn.ReadFromUDP(buf)
         if err != nil {
             fmt.Println("Error reading from UDP:", err)
             return
         }
+        usernameLen := buf[0]
+        username := buf[1:usernameLen+1]
+        message := buf[usernameLen+1:n]
 
         // 受信したデータを出力
         fmt.Printf("Received from %s\n", addr)
-        fmt.Printf("massage: %s\n", string(buf[:n]))
+        fmt.Printf("first byte: %d\n", usernameLen)
+        fmt.Printf("username: %s\n", string(username))
+        fmt.Printf("massage: %s\n", string(message))
 
-        // クライアントにメッセージを送信
-        message := []byte("Hello from server")
-        // メッセージが4096バイト以上の場合はエラーを表示
         if len([]byte(message)) > 4096 {
             fmt.Println("Error: Message is too large. Please enter a message of 4096 bytes or less.")
             continue
@@ -64,13 +67,17 @@ func main() {
         // クライアントの情報を保存
         clientInfos[addr.String()] = &ClientInfo{
             Address: addr,
+            Username: username,
+            Message: message,
             ReceivedTime: time.Now(),
         }
         for key, value := range clientInfos {
+            // 一定時間経過したクライアント情報を削除
             if time.Since(value.ReceivedTime) > 100 * time.Second {
                 delete(clientInfos, key)
             }
-            fmt.Println("Client addr: ", value.Address, "Client time: ", value.ReceivedTime)
+            fmt.Println("Client addr:", value.Address, "User:", string(value.Username))
+            fmt.Println("Message:", string(value.Message))
         }
         fmt.Printf("Number of clients: %d\n", len(clientInfos))
     }
