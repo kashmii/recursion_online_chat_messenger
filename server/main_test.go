f875cbe5f1ad5ee04f0cc310f08ca37c7a014036
kashmii <www.myac@gmail.com>
2024-05-12 11:19:16 +0900

    不要になったテストを削除

--- server/main_test.go
+++ server/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net"
 	"testing"
-	"time"
 )
 
 type MockPacketConn struct {
@@ -48,57 +47,5 @@ func TestMain(t *testing.T) {
     // TEST: message が正しくスライスから取得できているか
     if string(buf[len(username)+1:n]) != "test message" {
         t.Errorf("Expected %s, got %s", "test message", string(buf[len(username)+1:n]))
-    }
-}
-
-// TEST: uniqueAddrList関数 が重複のない正しいアドレスリストを返すか
-func TestUniqueAddrList(t *testing.T) {
-    dummyClient1 := &ClientInfo{
-        Address: &net.UDPAddr{
-            IP:   net.ParseIP("127.0.0.1"),
-            Port: 8080,
-        },
-        Username: []byte("dummyUser1"),
-        Message:  []byte("dummyMessage1"),
-        ReceivedTime: time.Now(),
-    }
-    dummyClient2 := &ClientInfo{
-        Address: &net.UDPAddr{
-            IP:   net.ParseIP("127.0.0.2"),
-            Port: 8081,
-        },
-        Username: []byte("dummyUser2"),
-        Message:  []byte("dummyMessage2"),
-        ReceivedTime: time.Now(),
-    }
-    dummyClient3 := &ClientInfo{
-        Address: &net.UDPAddr{
-            IP:   net.ParseIP("127.0.0.1"),
-            Port: 8080,
-        },
-        Username: []byte("dummyUser3"),
-        Message:  []byte("dummyMessage3"),
-        ReceivedTime: time.Now(),
-    }
-
-    clientInfos := make(map[string]*ClientInfo)
-    clientInfos[dummyClient1.Address.String()] = dummyClient1
-    clientInfos[dummyClient2.Address.String()] = dummyClient2
-    clientInfos[dummyClient3.Address.String()] = dummyClient3
-
-    // Call the uniqueAddrList function
-    addrList := uniqueAddrList(clientInfos)
-
-    if len(addrList) != 2 {
-        t.Errorf("Expected address list length of 2, got %d", len(addrList))
-    }
-
-    seen := make(map[string]bool)
-    for _, addr := range addrList {
-        addrStr := addr.String()
-        seen[addrStr] = true
-    }
-    if len(seen) != 2 {
-        t.Errorf("Expected 3 unique addresses, got %d", len(seen))
     }
 }
